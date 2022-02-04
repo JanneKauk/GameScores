29772904cc59bb65712cc12a33e5fa5c8ec200df
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-02-04 16:01:49 +0200

    Testing connection to REST API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+
+
+interface Game {
+    title: string;
+    description: string;
+}
 
 function App() {
+    const [games, setGames] = useState<Game[]>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/games').then(response => {
+            console.log("response");
+            setGames(response.data);
+        }
+        ).catch(err => console.log(err.message))
+    }, [])
+    if(games != null) {
+        console.log(games);
+    }
   return (
     <div className="App">
       <header className="App-header">
-        Header
+          Header
       </header>
     </div>
   );
