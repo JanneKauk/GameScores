6579823acff0b0191c9379b8cb76138f05b2289e
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-02-14 23:27:23 +0200

    Fixed circle

--- src/components/ScoreCircle.tsx
+++ src/components/ScoreCircle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useLayoutEffect, useRef} from "react";
 import '../scoreCircle.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -25,10 +25,33 @@ interface IProps{
 }
 
 const ScoreCircle: React.FC<IProps> = ({game}): JSX.Element => {
-    function adjustScore() {
+        const ratingRef = useRef<HTMLDivElement>(null);
+
+        useLayoutEffect(() => {
+            console.log(ratingRef);
+        })
+    const renderList = (): JSX.Element => {
+
+        return (
+            <div className="container center">
+                <div className="rating" ref={ratingRef}>
+                <span>
+                    {game.OverallScore}
+                    {/*{adjustScore()}*/}
+                </span>
+                </div>
+            </div>
+        )
+    }
+
+    function adjustScore () {
+        console.log(ratingRef.current);
+        console.log(ratingRef);
+        console.log("ei menty ohi")
         let temp;
         try {
-            temp = document.getElementById("game.Id");
+            temp = ratingRef.current;
+
             // Get content and get score as an int
             let ratingContent = game.OverallScore;
             let ratingScore = ratingContent*10;
@@ -37,35 +60,36 @@ const ScoreCircle: React.FC<IProps> = ({game}): JSX.Element => {
             let scoreClass = ratingScore < 40 ? "bad" : ratingScore < 60 ? "meh" : "good";
 
             // Add score class to the rating
-            temp!.classList.add(scoreClass);
+            ratingRef!.current!.classList.add(scoreClass);
 
             // After adding the class, get its color
-            const ratingColor = window.getComputedStyle(temp!).backgroundColor;
+            const ratingColor = window.getComputedStyle(ratingRef!.current!).backgroundColor;
 
             // Define the background gradient according to the score and color
             const gradient = `background: conic-gradient(${ratingColor} ${ratingScore}%, transparent 0 100%)`;
 
             // Set the gradient as the rating background
-            temp!.setAttribute("style", gradient);
+            ratingRef!.current!.setAttribute("style", gradient);
 
             // Wrap the content in a tag to show it above the pseudo element that masks the bar
-            temp!.innerHTML = `${ratingScore}`;
+            ratingRef!.current!.innerHTML = `<span>${ratingScore}</span>`;
         }catch (e){
             console.error(e);
             return;
         }
     }
 
+        useLayoutEffect(() => {
+            adjustScore();
+        })
+
     return (
-        <div className="container center">
-            <div id={"game.Id"} className="rating bad">
-                <span>
-                    {game.OverallScore}
-                    {adjustScore()}
-                </span>
-            </div>
+        <div >
+            {renderList()}
         </div>
     )
+
+
 }
 
 export default ScoreCircle
