1de06bf2b95db936aead3cfda419a526ad8a4bc6
Janne Kaukua <jannkauk@metropolia.fi>
2022-02-24 17:45:27 +0200

    Header changes with login/signup.

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -17,19 +17,17 @@ export const Header = (props: HeaderProps) => {
             </div>
             <nav className={styles.nav}>
                 <button style={buttStyle} onClick={onClick}>Login/Signup</button>
-                {showMenu && <div className="list-group-item" style={divStyle}>
-                    <h2>Login</h2>
-                    <div>
-                        <div>
-                            <input type={"text"} placeholder="Username"/>
-                        </div>
-                        <div>
-                            <input type="password" placeholder="Password"/>
-                        </div>
-                        <div>
-                            <button type="button" className="btn btn-danger">Submit</button>
-                            <button type="button" className="btn">Signup for a new account</button>
-                        </div>
+                {showMenu && <div className={styles.loginMenu} style={divStyle}>
+                    <h4 style={{margin: "1rem 0 0 0"}}>Login</h4>
+                    <div style={{margin: "0.5rem 0 1.5rem 0", display: "grid"}}>
+                        <label style={{textAlign:"left"}}>Username</label>
+                        <input type={"text"} placeholder="ILikeTrains58"/><br/>
+                        <label style={{textAlign:"left", margin:"0.5rem 0 0 0"}}>Password</label>
+                        <input type="password" placeholder="Something123"/>
+                    </div>
+                    <div style={{width: "100%", display: "flex"}}>
+                        <button type="button" className="btn btn-danger" style={{float: "left"}}>Submit</button>
+                        <button type="button" className={styles.buttonLink}>Signup for a new account</button>
                     </div>
                 </div>}
             </nav>
@@ -42,7 +40,7 @@ const divStyle: CSS.Properties = {
     position: "absolute",
     right: "1rem",
     zIndex: 10,
-    padding: "2rem"
+    padding: "0 2rem 2rem 2rem"
 }
 const buttStyle: CSS.Properties = {
     background:"none",
