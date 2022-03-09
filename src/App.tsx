33a4e4394693f6107c0337ea09ca5ea7b818686f
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-03-09 13:17:05 +0200

    Last

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ function App() {
     const [page, SetPage] = useState<number>(1)
     const [gameCount, setCount] = useState<number>(0)
     const [username, setUsername] = useState<string>('');
+    const [filteredGames, setFilteredGames] = useState<Games["game"]>([]);
+
 
     useEffect(() => {
         if(sessionStorage.getItem('username')) {
@@ -25,6 +27,7 @@ function App() {
         ).catch(err => console.log(err.message))
         axios.get('http://localhost:3001/games/everything/'+ page).then(response => {
             setGames(response.data);
+            setFilteredGames(response.data);
             console.log(page)
         }
         ).catch(err => console.log(err.message))
@@ -32,21 +35,44 @@ function App() {
     if(games != null) {
         console.log(games);
     }
+    const filterHandler = (search: string) => {
+        const filter = games.filter(game => {
+            if(game.title.includes(search)) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        console.log(filter);
+        // console.log(filteredGames + " search " + search);
+        if(filteredGames.length === 0) {
+            setFilteredGames(filter);
+            return;
+        }
+          setFilteredGames(filter);
+
+    }
     if(gameCount != 0){
         console.log("game count returns: "+gameCount)
     }
-    if(games!.length > 0 && games!==null){
+    if(games!.length > 0 && games!==null && filteredGames.length > 0){
         return (
             <div className="App">
-                <Header title="LOGO" username={username} />
+                <Header title="LOGO" username={username} filterHandler={filterHandler}  />
                 <Routes>
-                    <Route path='/' element={<List games={games} setPage={SetPage} page={page} gameCount={gameCount}/>}/>
+                    <Route path='/' element={<List games={filteredGames} setPage={SetPage} page={page} gameCount={gameCount}/>}/>
                     <Route path='/gamedetails/:id' element={<GameDetails title="Game Details" />} />
                 </Routes>
             </div>
         );
     }
-    return <div>a</div>;
+    return  <div className="App">
+        <Header title="LOGO" username={username} filterHandler={filterHandler}  />
+        <Routes>
+            {/*<Route path='/' element={<List games={games} setPage={SetPage} page={page} gameCount={gameCount}/>}/>*/}
+            <Route path='/gamedetails/:id' element={<GameDetails title="Game Details" />} />
+        </Routes>
+    </div>
 }
 
 interface Games {
--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -7,15 +7,22 @@ import LoginMenu from "../components/LoginMenu";
 interface HeaderProps {
     title: string;
     username: string;
+    filterHandler: (search: string) => void;
 }
 export const Header = (props: HeaderProps) => {
     const [showMenu, setShow] = useState(false)
     const onClick = () => setShow(!showMenu)
+    const [search, setSearch] = useState<string>('');
+    const filter = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+        props.filterHandler(event.target.value);
+    }
+
     return (
         <header className="App-header">
             <NavLink className={styles.title} to="/">{props.title}</NavLink>
             <div className={styles.searchdiv} >
-                <input type="search" />
+                <input type="search" onChange={filter} />
             </div>
             <nav className={styles.nav} style={{margin:"0 1rem 0 0"}}>
                 {props.username.length === 0 && <button style={buttStyle} onClick={onClick}>Login/Signup</button>}
